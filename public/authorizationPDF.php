120abaa9b9d610a7e0577ad0ba1b1d4381d90a24
Tim Hawkins <timhawkins@ibgsoftware.com>
2018-03-02 12:40:12 -0500

    disable pdf page for now

--- public/authorizationPDF.php
+++ public/authorizationPDF.php
@@ -1,25 +1,25 @@
 <?php
-require '../vendor/autoload.php';
-use Firebase\FirebaseLib;
+// require '../vendor/autoload.php';
+// use Firebase\FirebaseLib;
 
-$firebase = new \Firebase\FirebaseLib('https://scoopm-8975f.firebaseio.com/', $_COOKIE['userIDToken']);
+// $firebase = new \Firebase\FirebaseLib('https://scoopm-8975f.firebaseio.com/', $_COOKIE['userIDToken']);
 
-$user = $firebase->get('users/' . $_GET['id']);
-$user = json_decode($user);
+// $user = $firebase->get('users/' . $_GET['id']);
+// $user = json_decode($user);
 
-//CHECK THEY'RE ALLOWED TO VIEW THE FILE
-if ($user->error) {
-    header('Location: /');
-    die();
-}
+// //CHECK THEY'RE ALLOWED TO VIEW THE FILE
+// if ($user->error) {
+//     header('Location: /');
+//     die();
+// }
 
-//CREATE PDF
-$proof = new ProofOfAuthorization(
-    $user->signature,
-    $user->signatureTimestamp,
-    $user->signatureIP,
-    $user->signatureIPFromHeaders
-);
+// //CREATE PDF
+// $proof = new ProofOfAuthorization(
+//     $user->signature,
+//     $user->signatureTimestamp,
+//     $user->signatureIP,
+//     $user->signatureIPFromHeaders
+// );
 
-//DISPLAY PDF
-$proof->Output();
+// //DISPLAY PDF
+// $proof->Output();
